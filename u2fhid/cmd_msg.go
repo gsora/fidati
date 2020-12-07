5d0268ed75280c82cf13553ed2e0153add11587b
Gianguido Sorà <me@gsora.xyz>
2020-12-07 19:40:03 +0100

    pass up to session.total bytes to token
    handle corner case where session.data contains less than 57 actual
    bytes, which leads to wasteful padding passing

--- u2fhid/cmd_msg.go
+++ u2fhid/cmd_msg.go
@@ -3,7 +3,7 @@ package u2fhid
 // handleMsg handles cmdMsg commands.
 func (h *Handler) handleMsg(session *session, pkt u2fPacket) ([][]byte, error) {
 	return genPackets(
-		h.token.HandleMessage(session.data),
+		h.token.HandleMessage(session.data[:session.total]),
 		session.command,
 		pkt.ChannelBytes(),
 	)
