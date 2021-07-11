c6f14842d79cf1aeb8d336630c0243834307cc35
Gianguido Sorà <me@gsora.xyz>
2021-07-11 15:56:45 +0200

    send session id to HandleMessage from u2fhid handler

--- u2fhid/cmd_msg.go
+++ u2fhid/cmd_msg.go
@@ -3,7 +3,7 @@ package u2fhid
 // handleMsg handles cmdMsg commands.
 func (h *Handler) handleMsg(session *session, pkt u2fPacket) ([][]byte, error) {
 	return genPackets(
-		h.token.HandleMessage(session.data[:session.total]),
+		h.token.HandleMessage(session.data[:session.total], pkt.Channel()),
 		session.command,
 		pkt.ChannelBytes(),
 	)
